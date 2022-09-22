bd714cbda5b7a9ec4db7f321103db1d49f82746a
Isaac Pinto <isaacpinto@Isaacs-MacBook-Air.local>
2022-09-21 20:37:58 -0700

    add artist profile templates

--- src/Components/Pages/Artists/Artists.js
+++ src/Components/Pages/Artists/Artists.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './Artists.css'
+import Profile from '../../Profile/Profile'
 
 const Artists = () =>{
 
     return(
-        <div>
-            <div id = "artistgrid"></div>
+        <div id = "artistcont">
+            <div id = "artistgrid">
+                <div className='row' id = "row1">
+                    <Profile/>
+                    <Profile/>
+                    <Profile/>
+                    <Profile/>
+                </div>
+                <div className='row' id = "row2">
+                    <Profile/>
+                    <Profile/>
+                    <Profile/>
+                    <Profile/>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Profile.css'
+
+const Profile = () =>{
+
+    return (
+        <div className="cont grow">
+            <img id = "artistimage"></img>
+            <p id = "caption"></p>
+        </div>
+    )
+}
+
+export default Profile
