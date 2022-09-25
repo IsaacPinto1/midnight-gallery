399728f065892550be99670a7d1bd6574995af64
Isaac Pinto <isaacpinto@Isaacs-MacBook-Air.local>
2022-09-24 17:27:24 -0700

    add gallery art titles

--- src/Components/Art/Art.js
+++ src/Components/Art/Art.js
@@ -7,7 +7,10 @@ const Art = (props) =>{
     return (
         <div id = {props.id} className='art grow'>
                 <div id = "art-and-text">
-                    <img id = "image" src={require(`../../Images/Art/${props.image}`)}></img>
+                    <div id = "image-and-name">
+                        <img id = "image" src={require(`../../Images/Art/${props.image}`)}></img>
+                        <p id = "title">{props.title}</p>
+                    </div>
                     <div id = "desc">
                         <p id = "text">{props.text}</p>                    
                     </div>
