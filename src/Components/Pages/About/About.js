7a444b48a15d607328d3e2dfb4494d579d632d42
Isaac Pinto <isaacpinto@Isaacs-MacBook-Air.local>
2022-09-25 21:36:37 -0700

    add about page template

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './About.css'
 
 const About = () =>{
 
     return(
-        <div>
-            <h1>About</h1>
+        <div id = "testing">
+            <div id = "about-background"></div>
+            <div id = "about-main-text">About Us</div>
+            <div id = "about-text">qeirfjpeqhrfpqerfioequhf oeifrhoiqeuhrf oiueqhrf iouhe qfriouh oeqiurfh oiuhoiuqehroif hoiuh oiuhoiueqhrf oiuhoiuhoiueqhrfoiu h</div>
         </div>
     )
 }
