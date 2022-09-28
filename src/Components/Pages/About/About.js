ca091204cd947c6a160f752f0234fa8924cdb0b4
Isaac Pinto <isaacpinto@Isaacs-MacBook-Air.local>
2022-09-27 21:05:08 -0700

    add about page text and hover effects

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -4,10 +4,17 @@ import './About.css'
 const About = () =>{
 
     return(
-        <div id = "testing">
+        <div>
             <div id = "about-background"></div>
-            <div id = "about-main-text">About Us</div>
-            <div id = "about-text">qeirfjpeqhrfpqerfioequhf oeifrhoiqeuhrf oiueqhrf iouhe qfriouh oeqiurfh oiuhoiuqehroif hoiuh oiuhoiueqhrf oiuhoiuhoiueqhrfoiu h</div>
+            <div id = "about-main-text" className="glow">About Us</div>
+            <div id = "about-text">We are the Midnight Gallery, a collection of 
+            artwork from mostly unknown artists. Our name comes from the
+            idea of 'moonlighting', or having a second job in addition to one's main
+            form of employment. Everyone featured in the Midnight Gallery 'moonlights' 
+            as an artist and our gallery represents a space for these non-professional
+            artists to display their work. Our hope is to bring a meaningful level of
+            exposure to artists that wouldn't normally get the chance to display their 
+            work in a gallery setting. We hope you enjoy their work.</div>
         </div>
     )
 }
