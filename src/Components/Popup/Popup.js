408840e4e817781c73a79118d8c16bfc4b46eafc
Isaac Pinto <isaacpinto@Isaacs-MacBook-Air.local>
2022-09-28 12:48:57 -0700

    add cross to artist popups to close window

--- src/Components/Popup/Popup.js
+++ src/Components/Popup/Popup.js
@@ -7,13 +7,16 @@ const Popup = (props) =>{
         <div>
             <div onClick={() =>props.function("none")} id = "black"></div>
             <div id = "window">
-                <p id = "artistdesc">
-                    <div id = "snapshot">
-                        <img id = "headshot" src={require(`../../Images/Artists/${props.artist}`)}></img>
-                        <div id = "name">{props.name}</div>
-                    </div>
-                </p>
-                <p id = "artistdesc">{props.desc}</p>
+                <div id = "window-content">
+                    <div id = "cross" onClick = {()=>props.function("none")}>X</div>
+                    <p id = "artistdesc">
+                        <div id = "snapshot">
+                            <img id = "headshot" src={require(`../../Images/Artists/${props.artist}`)}></img>
+                            <div id = "name">{props.name}</div>
+                        </div>
+                    </p>
+                    <p id = "artistdesc">{props.desc}</p>
+                </div>
             </div>
         </div>
     )
