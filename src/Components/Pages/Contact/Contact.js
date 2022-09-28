61cac443e709f5e3851c4a29c4056dff84181593
Isaac Pinto <isaacpinto@Isaacs-MacBook-Air.local>
2022-09-27 21:23:17 -0700

    add contact page and update glow effects for about and contact

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './Contact.css'
 
 const Contact = () =>{
 
     return(
         <div>
-            <h1>Contact</h1>
+            <div id = "contact-background"></div>
+            <div id = "contact-main-text" className="glow">Contact Us</div>
+            <div id = "contact-text">
+                Phone Number: (310) 666-4290 <br></br>
+                Email: isaac.pinto2003@gmail.com
+            </div>
         </div>
     )
 }
